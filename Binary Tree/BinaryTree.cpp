274aa95748a972089521127ffec314afc062db81
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-18 21:31:33 +0700

    height of tree

--- Binary Tree/BinaryTree.cpp
+++ Binary Tree/BinaryTree.cpp
@@ -65,6 +65,79 @@ class CNode {
             }
         }
 
+        void LNR() {
+            if (_data != -1) {
+                if (_pLeft != NULL) {
+                    _pLeft->LNR();
+                }
+                cout << _data << " ";
+                if (_pRight != NULL) {
+                    _pRight->LNR();
+                }
+            }
+        }
+
+        void LRN() {
+            if (_data != -1) {
+                if (_pLeft != NULL) {
+                    _pLeft->LRN();
+                }
+                if (_pRight != NULL) {
+                    _pRight->LRN();
+                }
+                cout << _data << " ";
+            }
+        }
+
+        CNode* search(int x) {
+            if (x == _data) {
+                return this;
+            }
+            if (x > _data && _pRight != NULL) {
+                return _pRight->search(x);
+            }
+            if (x < _data && _pLeft != NULL) {
+                return _pLeft->search(x);
+            }
+            return NULL;
+        }
+
+        int height() {
+            if (_pLeft == NULL && _pRight == NULL) {
+                if (_data == -1) {
+                    return 0;
+                }
+                return 1;
+            }
+            int left = 0, right = 0;
+            if (_pLeft != NULL) {
+                left = _pLeft->height();
+            }
+            if (_pRight != NULL) {
+                right = _pRight->height();
+            }
+            if (left > right) {
+                return left + 1;
+            }
+            return right + 1;
+        }
+
+        // void remove(int x) {
+        //     if (x > _data && _pRight != NULL) {
+        //         _pRight->remove(x);
+        //     }
+        //     else if (x < _data && _pLeft != NULL) {
+        //         _pLeft->remove(x);
+        //     }
+        //     else {
+        //         CNode *del = this;
+        //         if (del->_pLeft == NULL && del->_pRight == NULL) {
+        //             delete del;
+        //             del = NULL;
+        //         }
+        //     }
+        // }
+
     private:
         int _data;
         CNode *_pLeft;
@@ -82,7 +155,8 @@ int main(int argc, char const *argv[]) {
     myTree->addNode(5);
     myTree->addNode(4);
 
-    myTree->NLR();
+    cout << myTree->search(6)->getData() << endl;
+    cout << myTree->height() << endl;
 
     return 0;
 }
